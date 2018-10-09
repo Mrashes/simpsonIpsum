75dec2fceb8c020df255143faa0f8f5035b54030
Rashes, Max (mr050n) <mrashes2@gmail.com>
2018-10-08 21:11:55 -0500

    Wbell assisted with figuring out how I'm a dummy

--- generator.js
+++ generator.js
@@ -1,63 +1,57 @@
+'use strict';
 // Code that generates the random lorem ipsum text
 
 // Create a new object called loremIpsum by invoking the GenerateNewText constructor function
-const quotes = require("./simpsonQuotes")
-const ralphQuotes = require("./ralphQuotes")
+const quotes = require('./simpsonQuotes');
+const ralphQuotes = require('./ralphQuotes');
 const loremIpsum = new GenerateNewText();
 
 // Constructor function that creates an object with the sentences property
 function GenerateNewText() {
   // Add property to the object
-  this.sentences = ["Simpson, Homer Simpson. He's the greatest guy in history. From the town of Springfield, he's about to hit a chestnut tree. AAH!", 
-  ...quotes,
-  ...ralphQuotes];
+  this.sentences = [`Simpson, Homer Simpson. He's the greatest guy in history. From the town of Springfield, he's about to hit a chestnut tree. AAH!`,
+    ...quotes,
+    ...ralphQuotes];
 }
 
 // Method to the GenerateNewText constructor function that generates a random sentence
 GenerateNewText.prototype.getRandomSentence = function() {
-  let randomSentence = this.sentences[Math.floor(Math.random() * this.sentences.length)]
-  if(randomSentence === undefined) {
-    console.log(undefined, "path")
-    console.log(randomSentence, "variable")
-    console.log(Math.floor(Math.random() * this.sentences.length), "math part/index")
-    console.log(this.sentences[Math.floor(Math.random() * this.sentences.length)], "check if undefined")
-  }
-	return randomSentence;
-}
+  let randomNumber = Math.floor(Math.random() * this.sentences.length);
+  let randomSentence = this.sentences[randomNumber];
+  return randomSentence;
+};
 
 // Method to the GenerateNewText constructor function that generates a paragraph from random sentences
 GenerateNewText.prototype.getParagraph = function() {
-  let paragraph = "";
+  let paragraph = '';
   // Set the minimum number of words
   let minimumCharacterLength = 250;
   let firstSentence = true;
   while (paragraph.length < minimumCharacterLength) {
-    var sentance = this.getRandomSentence()
-    if (sentance !== undefined) {
-      if (firstSentence) {
-        paragraph = paragraph.concat(sentance);
-        firstSentence = false;
-      } else {
-        paragraph = paragraph.concat(" " + sentance);
-      }
+    var sentance = this.getRandomSentence();
+    if (firstSentence) {
+      paragraph = paragraph.concat(sentance);
+      firstSentence = false;
+    } else {
+      paragraph = paragraph.concat(' ' + sentance);
     }
   }
   return paragraph;
-}
+};
 
 // Method to the GenerateNewText constructor function that gerates multiple paragraphs from paragraphs
 GenerateNewText.prototype.getAllParagraphs = function(numberOfParagraphs) {
-  let allParagraphs = [];
+  const allParagraphs = [];
   // Generate the number of paragraphs as specified by the user
   while (allParagraphs.length < numberOfParagraphs) {
     allParagraphs.push(this.getParagraph());
   }
   // Convert array into HTML string
-  let paragraphHTML = "";
+  let paragraphHTML = '';
   allParagraphs.forEach(function (paragraph) {
-    paragraphHTML += "<p>" + paragraph + "</p>";
+    paragraphHTML += '<p>' + paragraph + '</p>';
   });
   return paragraphHTML;
-}
+};
 
 module.exports = loremIpsum;
--- router.js
+++ router.js
@@ -1,3 +1,4 @@
+'use strict';
 // Require the necessary modules
 const loremIpsum = require("./generator.js");
 const querystring = require("querystring");
